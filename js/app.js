22f18b4e3e97845b0d6925a31a9d48fbfb28c383
3jtechno <3j.techno@gmail.com>
2019-01-04 21:44:39 +0000

    Creation of initial grid

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => console.log('HelloWorld'))
+const gridWidth = 10
+const gridHeight = 20
+
+function init(){
+
+  const grid = document.getElementById('grid')
+
+  for(let i = 0; i < gridWidth * gridHeight; i++){
+    const cell = document.createElement('div')
+    grid.appendChild(cell)
+  }
+
+  // const form = [5,6,15,16]
+  // form.forEach(formIndex => grid.classList.toggle('form'))
+  //
+
+}
+
+document.addEventListener('DOMContentLoaded', init)
