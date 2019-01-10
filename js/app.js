6620fbebc74bb303d9101cf97ccd7b44d16e3cb6
3jtechno <3j.techno@gmail.com>
2019-01-10 22:14:11 +0000

    Created Game class and refactor blinking line function

--- js/app.js
+++ js/app.js
@@ -1,29 +1,53 @@
-const gridWidth = 10
-const gridHeight = 20
-const speed = 1000
-const shapesList = [
-  { name: 'cube', coords: [14,4,5,15] },
-  { name: 'bar', coords: [4,3,5,6] },
-  { name: 'L', coords: [4,5,6,14] },
-  { name: 'inverted-L', coords: [6,4,5,16] },
-  { name: 'T', coords: [5,4,6,15] },
-  { name: 'S', coords: [15,5,6,14] },
-  { name: 'inverted-S',coords: [16,5,6, 17] }
-]
-const matrixOfRotation = [
-  0, -9, -18, -27, 0, 0, 0, 0, 29, 20, 11, 2, -7, 0, 0,
-  0, 0, 0, 0, 31, 22, 13, 0, 0, 0, 0, 0, 0, 0, 0, 33
-]
-const keysSets = [[32,37,39,40],[16,65,68,83]]
-let nbOfPlayer,fallTimerId,
-  boardBestScore, bestScore,
-  boardPlayer2,
-  player1Btn, player2Btn, startBtn, winner,
-  gameStarted
-let grids = []
+class Shape{
+  constructor(gridWidth){
+    this.gridWidth = gridWidth
+    this.shapesList = [
+      { name: 'cube', coords: [14,4,5,15] },
+      { name: 'bar', coords: [4,3,5,6] },
+      { name: 'L', coords: [4,5,6,14] },
+      { name: 'inverted-L', coords: [6,4,5,16] },
+      { name: 'T', coords: [5,4,6,15] },
+      { name: 'S', coords: [15,5,6,14] },
+      { name: 'inverted-S',coords: [16,5,6, 17] }
+    ]
+    this.matrixOfRotation = [
+      0, -9, -18, -27, 0, 0, 0, 0, 29, 20, 11, 2, -7, 0, 0,
+      0, 0, 0, 0, 31, 22, 13, 0, 0, 0, 0, 0, 0, 0, 0, 33
+    ]
+
+    const randomShape = Math.floor(Math.random() * this.shapesList.length)
+
+    this.name = this.shapesList[randomShape].name
+    this.position = this.shapesList[randomShape].coords.slice()
+  }
+
+  getNextPosition(direction){
+    let distance
+    return this.position.map(element => {
+      switch(direction){
+        case 'rotate':
+        //If first element (center of rotation) or element a squar do not rotate
+          if(element === this.position[0] || this.name === 'cube') return element
+          //Calculation of the distance between the center of rotation and the element to rotate
+          distance = this.position[0] - element
+          //Return the element new position using the rotation matrix
+          return distance >= 0 ? element + this.matrixOfRotation[distance]: element - this.matrixOfRotation[- distance]
+        case 'left':
+          return element - 1
+        case 'right':
+          return element + 1
+        case 'down':
+          return element + this.gridWidth
+      }
+    })
+  }
+
+}
 
 class Grid{
-  constructor(player, callback){
+  constructor(player, keysSet, addLineToOpponent, endGame){
+    this.gridWidth = 10
+    this.gridHeight = 20
     this.player = player
     this.shape
     this.score
@@ -31,8 +55,9 @@ class Grid{
     this.arrayKeyPressed = []
     this.gridFrame = document.getElementById(`grid${player}`)
     this.filling = new Array(20).fill(null).map(() => []) // ugly :(
-    this.addLineToOpponent = callback
-    this.gameControl = player === 1 && nbOfPlayer === 2 ? keysSets[1]:keysSets[0]
+    this.gameControl = keysSet
+    this.addLineToOpponent = addLineToOpponent //CallBack
+    this.endGame = endGame //CallBack
 
     //This bind the grid object when document is calling these function
     this.fall = this.fall.bind(this)
@@ -43,19 +68,18 @@ class Grid{
 
   init(){
     //Creation of the initial grid according to the player
-    for(let i = 0; i < gridWidth * gridHeight; i++){
+    for(let i = 0; i < this.gridWidth * this.gridHeight; i++){
       const cell = document.createElement('div')
       this.gridFrame.appendChild(cell)
     }
     this.gridCollection = Array.from(this.gridFrame.querySelectorAll('div'))
-    console.log(this.gridCollection)
     this.score = document.getElementById(`score${this.player}`)
     this.score.innerHTML = 0
 
     document.addEventListener('keydown', this.handleSimultaneousKeyDown)
     document.addEventListener('keyup', this.handleSimultaneousKeyDown)
 
-    this.shape = new Shape()
+    this.shape = new Shape(this.gridWidth)
 
   }
 
@@ -64,7 +88,7 @@ class Grid{
     const continueFall = this.move('down')
     //Stop the time if shape cannot move down anymore
     if(!continueFall && this.shape.position.includes(4)){
-      endGame(this.player, parseFloat(this.score.innerHTML))
+      this.endGame(this.player, parseFloat(this.score.innerHTML))
     } else if(!continueFall){
       this.nextShape()
     }
@@ -95,15 +119,15 @@ class Grid{
       // const nextIndex = getNextPosition(direction, index)
       const gridLength = this.gridCollection.length
       const currentIndex = this.shape.position[arrayIndex]
-      const currentIndexXPos =  currentIndex % gridWidth
-      const nextIndexXPos = nextIndex % gridWidth
+      const currentIndexXPos =  currentIndex % this.gridWidth
+      const nextIndexXPos = nextIndex % this.gridWidth
 
       //Check that next index in not colling with the grid filling
       const collideWithAnotherShape = this.contains(nextIndex)
       const reachedTopOrBottom = nextIndex < 0 || nextIndex >= gridLength
       //Check that your not on a side if you want to move left or right
       const cannotGoLeft = currentIndexXPos === 0 && direction === 'left'
-      const cannotGoRigth = currentIndexXPos === gridWidth - 1 && direction === 'right'
+      const cannotGoRigth = currentIndexXPos === this.gridWidth - 1 && direction === 'right'
       //During rotation, check that the next element didn't appear on the other side
       //if roation was being made too close from a border
       const cannotRotate = Math.abs(currentIndexXPos - nextIndexXPos) > 2
@@ -126,24 +150,49 @@ class Grid{
     //Add shape to the grid
     this.addShapeToGrid()
     //Make full lines blinking and remove them
-    this.blinkLine(this.removeFullLine())
+    const fullLines = this.findFullLines()
+    if(fullLines.length){
+      this.blinkFullLine(fullLines)
+    } else {
+      this.redraw()
+    }
   }
 
   addShapeToGrid(){
+    //Add element position to the grid.filling array
     this.shape.position.forEach(element => {
-      //Add a line in grid.filling if it doesn't exist yet (0 bottom to 19 top)
-      const elementLine = Math.floor(element / gridWidth)
-      //Add element position to the grid.filling array
+      const elementLine = Math.floor(element / this.gridWidth)
       this.filling[elementLine].push(element)
     })
   }
 
-  removeFullLine(){
-    const lineToBeRemoved = []
-    //If a line is completed, remove the line from the grid.filling array
+  findFullLines(){
+    const fullLines = []
     this.filling.forEach((line, index) => {
-      if(line.length === 10) lineToBeRemoved.push(index)
+      if(line.length === 10) fullLines.push(index)
     })
+    return fullLines
+  }
+
+  blinkFullLine(fullLines){
+    //Blink the full lines for half a second
+    fullLines.forEach(line => {
+      for(let i = 0; i < 10; i++){
+        this.gridCollection[String(line)+String(i)].classList.add('blink')
+      }
+    })
+    let count = 0
+    setInterval(() => {
+      if(count === 1) {
+        this.gridCollection.forEach(element =>  element.classList.remove('blink'))
+        this.removeFullLine()
+      }
+      count ++
+    },500)
+  }
+
+  removeFullLine(){
+    //Remove full line from the grid.filling array
     for(let i = this.filling.length - 1; i >= 0; i--){
       if(this.filling[i].length === 10){
         this.filling.splice(i,1)
@@ -152,32 +201,13 @@ class Grid{
         this.increaseScore()
         for(let j = i; j >= 0; j--){
           //Move the lines above down
-          this.filling[j].forEach((element, index) => this.filling[j][index] = element + gridWidth)
+          this.filling[j].forEach((element, index) => this.filling[j][index] = element + this.gridWidth)
         }
-        if(nbOfPlayer === 2) this.addLineToOpponent(this.player === 1 ? 2 : 1)
+        this.addLineToOpponent(this.player === 1 ? 2 : 1)
         i++
       }
     }
-    return lineToBeRemoved
-  }
-
-  blinkLine(lineToBeRemoved){
-    if(lineToBeRemoved.length === 0) return this.redraw()
-
-    lineToBeRemoved.forEach(line => {
-      for(let i = 0; i < 10; i++){
-        this.gridCollection[String(line)+String(i)].classList.add('blink')
-      }
-    })
-
-    let count = 0
-    setInterval(() => {
-      if(count === 1) {
-        this.gridCollection.forEach(element =>  element.classList.remove('blink'))
-        this.redraw()
-      }
-      count ++
-    },500)
+    this.redraw()
   }
 
   increaseScore(){
@@ -187,8 +217,7 @@ class Grid{
 
   nextShape(){
     this.updateGrid()
-    //Lose condition
-    this.shape = new Shape()
+    this.shape = new Shape(this.gridWidth)
     this.showShape()
   }
 
@@ -196,14 +225,14 @@ class Grid{
     //Generate a new line with random number of bricks
     const newLine = new Array()
     for(let i = 0; i < 10; i++){
-      const addAnBrick = Boolean(Math.round(Math.random()))
-      if(addAnBrick) newLine.push(190+i)
+      const addBrick = Boolean(Math.round(Math.random()))
+      if(addBrick) newLine.push(190+i)
     }
     //Remove top line
     this.filling.shift()
     //Move the lines up
     this.filling.forEach(line => {
-      line.forEach((element, index) => line[index] -= gridWidth)
+      line.forEach((element, index) => line[index] -= this.gridWidth)
     })
     //Insert new line at the bottom
     this.filling.push(newLine)
@@ -215,8 +244,7 @@ class Grid{
   redraw(){
     //Remove all css class shape
     this.gridCollection.forEach(element => element.className = '')
-    // this.gridCollection.forEach(element => element.classList.remove('stack'))
-    //Add css class shape according to grid.filling content
+    //Add css class stack according to grid.filling content
     this.filling.forEach(element => {
       element.forEach(element => this.gridCollection[element].classList.add('stack'))
     })
@@ -224,7 +252,6 @@ class Grid{
 
   destruct(){
     this.gridFrame.innerHTML = ''
-    clearInterval(fallTimerId)
   }
 
   handleKeydown(key){
@@ -241,9 +268,6 @@ class Grid{
       case this.gameControl[3]:
         this.move('down')
         break
-      case 80:
-        clearInterval(fallTimerId)
-        break
     }
   }
 
@@ -259,123 +283,101 @@ class Grid{
 
 }
 
-class Shape{
+class Game{
   constructor(){
-    this.name
-    this.color
-    this.position = []
+    this.nbOfPlayer = 1
+    this.speed = 1000
+    this.gameStarted = false
+    this.fallTimerId
+    this.keysSets = [[32,37,39,40],[16,65,68,83]]
+
+    this.grids = []
+    this.boardPlayer2 = document.querySelector('.player2')
+    this.boardBestScore  = document.querySelector('.bestScore')
+    this.player1Btn = document.getElementById('1')
+    this.player2Btn = document.getElementById('2')
+    this.startBtn = document.getElementById('start')
+    this.bestScore = document.getElementById('best-score')
+    this.winner = document.querySelector('.winner')
+    this.addLineToOpponent = this.addLineToOpponent.bind(this)
+    this.switchPlayer = this.switchPlayer.bind(this)
+    this.startGame = this.startGame.bind(this)
+
     this.init()
   }
 
   init(){
-    const randomShape = Math.floor(Math.random() * shapesList.length)
-    //careful here, do not do "this.position = shapeLi..." otherwise the shapesList will be updated as the shape moves.
-    this.name = shapesList[randomShape].name
-    this.position = shapesList[randomShape].coords.slice()
+    //Display user best score if any
+    this.bestScore.innerHTML = localStorage.getItem('tetris-best-score') || 0
+
+    this.player1Btn.addEventListener('click', this.switchPlayer)
+    this.player2Btn.addEventListener('click', this.switchPlayer)
+    this.startBtn.addEventListener('click', this.startGame)
   }
 
-  getNextPosition(direction){
-    let distance
-    return this.position.map(element => {
-      switch(direction){
-        case 'rotate':
-        //If first element (center of rotation) or element a squar do not rotate
-          if(element === this.position[0] || this.name === 'cube') return element
-          //Calculation of the distance between the center of rotation and the element to rotate
-          distance = this.position[0] - element
-          //Return the element new position using the rotation matrix
-          return distance >= 0 ? element + matrixOfRotation[distance]: element - matrixOfRotation[- distance]
-        case 'left':
-          return element - 1
-        case 'right':
-          return element + 1
-        case 'down':
-          return element + gridWidth
-      }
-    })
-  }
-
-}
-
-function endGame(player, score){
-  clearInterval(fallTimerId)
-  if(nbOfPlayer === 1){
-    if(score > bestScore.innerHTML){
-      localStorage.setItem('tetris-best-score', score)
-      bestScore.innerHTML = score
+  switchPlayer(e){
+    //Switch between 1 and 2 players view
+    if(!e.target.classList.contains('selected')){
+      this.nbOfPlayer = this.nbOfPlayer === 1 ? 2 : 1
+      this.player1Btn.classList.toggle('selected')
+      this.player2Btn.classList.toggle('selected')
+      this.boardBestScore.classList.toggle('hide')
+      this.boardPlayer2.classList.toggle('hide')
     }
-    winner.innerHTML = 'Game Over'
-  } else if(nbOfPlayer === 2){
-    winner.innerHTML = `Player ${player === 1 ? 2 : 1} Lose`
   }
-  winner.classList.add('display-score')
-}
 
-function reinitiateGrid(){
-  //Kill grid objects and reinitialise grids
-  grids.forEach(element => element.destruct())
-  grids = new Array()
-  winner.innerHTML = ''
-  winner.classList.remove('display-score')
-}
-
-function addLineToOpponent(player){
-  grids[player-1].addOneLine()
-}
-
-function createGrids(){
-  //Create grids based on number of player
-  for(let i = 0; i < nbOfPlayer; i++){
-    grids.push(new Grid(i+1, addLineToOpponent))
+  startGame(){
+    if(!this.gameStarted){
+      this.createGrids(this.nbOfPlayer)
+    } else {
+      this.reinitiateGrid()
+    }
+    this.startBtn.innerHTML = this.startBtn.innerHTML === 'Start'? 'Stop' : 'Start'
+    this.player1Btn.disabled = !this.player1Btn.disabled
+    this.player2Btn.disabled = !this.player2Btn.disabled
+    //Unfocus the start button to avoid interction when spacebar key is pressed
+    this.startBtn.blur()
+    this.gameStarted = !this.gameStarted
   }
-  fallTimerId = setInterval(() => {
-    grids.forEach(grid => grid.fall())
-  },speed)
-}
 
-function startGame(){
-  if(!gameStarted){
-    createGrids(nbOfPlayer)
-  } else {
-    reinitiateGrid()
+  createGrids(){
+    //Create grids based on number of player
+    for(let i = 0; i < this.nbOfPlayer; i++){
+      const keysSet = i === 0 && this.nbOfPlayer === 2 ? this.keysSets[1]: this.keysSets[0]
+      this.grids.push(new Grid(i+1, keysSet, this.addLineToOpponent, this.endGame))
+    }
+    this.fallTimerId = setInterval(() => {
+      this.grids.forEach(grid => grid.fall())
+    },this.speed)
   }
-  startBtn.innerHTML = startBtn.innerHTML === 'Start'? 'Stop' : 'Start'
-  player1Btn.disabled = !player1Btn.disabled
-  player2Btn.disabled = !player2Btn.disabled
-  //Unfocus the start button to avoid interction when spacebar key is pressed
-  startBtn.blur()
-  gameStarted = !gameStarted
-}
 
-function switchPlayer(e){
-  //Switch between 1 and 2 players view
-  if(!e.target.classList.contains('selected')){
-    nbOfPlayer = nbOfPlayer === 1 ? 2 : 1
-    player1Btn.classList.toggle('selected')
-    player2Btn.classList.toggle('selected')
-    boardBestScore.classList.toggle('hide')
-    boardPlayer2.classList.toggle('hide')
+  endGame(player, score){
+    clearInterval(this.fallTimerId)
+    if(this.nbOfPlayer === 1){
+      if(this.score > this.bestScore.innerHTML){
+        localStorage.setItem('tetris-best-score', this.score)
+        this.bestScore.innerHTML = score
+      }
+      this.winner.innerHTML = 'Game Over'
+    } else if(this.nbOfPlayer === 2){
+      this.winner.innerHTML = `Player ${player === 1 ? 2 : 1} Lose`
+    }
+    this.winner.classList.add('display-score')
   }
+
+  reinitiateGrid(){
+    //Kill grid objects and reinitialise grids
+    clearInterval(this.fallTimerId)
+    this.grids.forEach(element => element.destruct())
+    this.grids = new Array()
+    this.winner.innerHTML = ''
+    this.winner.classList.remove('display-score')
+  }
+
+  addLineToOpponent(player){
+    if(this.nbOfPlayer === 2) this.grids[player-1].addOneLine()
+  }
+
 }
 
-function init(){
-  //Set default nb of player to 1
-  nbOfPlayer = 1
-
-  boardBestScore  = document.querySelector('.bestScore')
-  boardPlayer2 = document.querySelector('.player2')
-  player1Btn = document.getElementById('1')
-  player2Btn = document.getElementById('2')
-  startBtn = document.getElementById('start')
-  bestScore = document.getElementById('best-score')
-  winner = document.querySelector('.winner')
-
-  //Display user best score if any
-  bestScore.innerHTML = localStorage.getItem('tetris-best-score') || 0
-
-  player1Btn.addEventListener('click', switchPlayer)
-  player2Btn.addEventListener('click', switchPlayer)
-  startBtn.addEventListener('click', startGame)
-}
-
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener('DOMContentLoaded', () => new Game)
